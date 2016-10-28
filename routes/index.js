63bbe36f60f3c7cc81015b9e1e58bbb4e4ad9b46
muggezifter <muggezifter@gmail.com>
2016-10-28 14:26:41 +0200

    use static index.html

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,6 @@ var player = require('../controllers/play');
 
 
 /**
- * Show ui
- */
-router.get('/',(req, res) => res.render('index', { title: 'KHL4 grid' }));
-
-/**
  * Play a chord
  */
 router.get("/play",(req, res) => player.play(req, res));
