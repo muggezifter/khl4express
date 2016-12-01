0e612bf6e6922846226b2fda8fce0d77e5900604
muggezifter <muggezifter@gmail.com>
2016-12-01 21:00:13 +0100

    updated package.json

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ var grid = require('./routes/grid');
 
 var app = express();
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// set routes
 app.use('/', routes);
 app.use('/test', test);
 app.use('/recording', recording);
